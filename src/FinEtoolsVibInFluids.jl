7693674361d22846e49e06d10ba4f385b6bb2990
Petr Krysl <pkrysl@ucsd.edu>
2020-01-20 19:22:48 -0800

    top module

---
+++ src/FinEtoolsVibInFluids.jl
@@ -0,0 +1,13 @@
+"""
+FinEtools (C) 2017-2020, Petr Krysl
+
+Finite Element tools.  Julia implementation  of the finite element method
+for continuum mechanics. Package for problems of vibration in fluids.
+"""
+module FinEtoolsVibInFluids
+
+__precompile__(false)
+
+include("LaplBEM.jl")
+
+end # module
