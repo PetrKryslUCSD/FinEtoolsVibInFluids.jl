20b9292880b7e355eda77989df2df0687805e8a4
Petr Krysl <pkrysl@ucsd.edu>
2020-07-15 12:40:40 -0700

    example with coarsening/refinement

--- examples/flint_rock3_examples.jl
+++ examples/flint_rock3_examples.jl
@@ -1,10 +1,12 @@
 module flint_rock3_examples
 using FinEtools
 using FinEtools.MeshImportModule
+using FinEtools.MeshExportModule
 using FinEtoolsDeforLinear
 using FinEtoolsDeforLinear.AlgoDeforLinearModule
 using FinEtoolsVibInFluids.LaplBEM
 using FinEtoolsVibInFluids.AlgoVibInFluidsModule
+using FinEtoolsVoxelMesher.TetRemeshingModule
 using LinearAlgebra
 using TimerOutputs
 using Arpack
@@ -252,6 +254,196 @@ function flint_rock3_wet_algo()
 	modeldata = AlgoVibInFluidsModule.exportmode(modeldata)
     true
 end # flint_rock3_wet_algo
+
+function free_vibration_solver_w_remeshing()
+	# Free-vibration solution for a clamped plate 
+	# Reference: Fu, Y., and Price, W. G., 1987, “Interactions Between a Partially 
+	# or Totally Immersed Vibrating Cantilever Plate and the Surrounding Fluid,” 
+	# J. Sound Vib., 118(3), pp. 495–513.
+	# Dry Natural frequencies [radians per second] (nLength=10,nWidth=10)
+	# 12.45153      29.44122      75.04792      94.26498      106.7881
+	to = TimerOutput()
+	
+	println("Loading mesh")
+    @timeit to "Loading mesh" begin
+	    MR = DeforModelRed3D
+	    output = MeshImportModule.import_ABAQUS("./flint-rock3-1_5mm.inp")
+	    fens, fes = output["fens"], output["fesets"][1]
+	    fens.xyz .*= phun("MM")
+	end
+
+    println("Before unrefinement: ")
+    println("Number of nodes: $(count(fens))")
+    println("Interior mesh: $(count(fes)) tets")
+    println("Surface mesh: $(count(meshboundary(fes))) triangles")
+
+    File = "Original.vtk"
+    vtkexportmesh(File, fens, fes)
+    # @async run(`"paraview.exe" $File`)
+
+    for pass in 1:4
+    	nt = connasarray(fes)
+    	nv = deepcopy(fens.xyz)
+    	ntmid = [1 for idx in 1:size(nt, 1)] 
+    	nt, nv, ntmid = TetRemeshingModule.coarsen(nt, nv, ntmid; desired_ts = 1.25^(pass-1) * 0.001, surface_coarsening = false)
+    	nt, nv, ntmid = TetRemeshingModule.coarsen(nt, nv, ntmid; desired_ts = 1.25^(pass-1) * 0.001, surface_coarsening = true)
+    	
+    	fens.xyz = nv
+    	fes = fromarray!(fes, nt)
+    	setlabel!(fes, ntmid)
+    	# fens = meshsmoothing(fens, fes; method = :taubin, npass = 1)
+
+    	File = "Unref-$(pass).vtk"
+    	vtkexportmesh(File, fens, fes)
+    	# @async run(`"paraview.exe" $File`)
+
+    	println("After unrefinement: ")
+    	println("Number of nodes: $(count(fens))")
+    	println("Interior mesh: $(count(fes)) tets")
+    	println("Surface mesh: $(count(meshboundary(fes))) triangles")
+    end
+
+    fens, fes = T4refine(fens, fes)
+
+    File = "Ref.vtk"
+    vtkexportmesh(File, fens, fes)
+    # @async run(`"paraview.exe" $File`)
+
+    println("After refinement: ")
+    println("Number of nodes: $(count(fens))")
+    println("Interior mesh: $(count(fes)) tets")
+    println("Surface mesh: $(count(meshboundary(fes))) triangles")
+
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(size(fens.xyz,1),3)) # displacement field
+    # nl = selectnode(fens, box=[0.0 0.0 -Inf Inf -Inf Inf], inflate=tolerance)
+    # setebc!(u, nl, true, collect(1:3))
+    applyebc!(u)
+    numberdofs!(u)
+
+    material = MatDeforElastIso(MR, rho, E, nu, 0.0)
+
+    @timeit to "Set up FE machine" begin
+	    femm = FEMMDeforLinearESNICET4(MR, IntegDomain(fes, NodalSimplexRule(3)), material)
+	    associategeometry!(femm,  geom)
+	end
+
+	println("Stiffness matrix")
+    @timeit to "Stiffness matrix" begin
+	    K = stiffness(femm, geom, u)
+	end
+	println("Mass matrix")
+	@timeit to "Mass matrix" begin
+    	M = mass(femm, geom, u)
+    end
+
+    println("Eigenvalue problem")
+    @timeit to "Eigenvalue problem" begin
+	    d,v,nev,nconv = eigs(K+OmegaShift^2*M, M; nev=neigvs, which=:SM)
+	    d = d .- OmegaShift^2;
+	end
+
+	println("Done")
+
+	return Dict("fens"=>fens, "femm"=>femm, "geom"=>geom, "u"=>u, "eigenvectors"=>v, "eigenvalues"=>d, "timeroutput" =>to)
+end # free_vibration_solver
+
+function flint_rock3_wet_unrefine()
+	to = TimerOutput()
+
+	model = free_vibration_solver_w_remeshing()
+
+	@show model["timeroutput"]
+
+	fens = model["fens"]
+	femm = model["femm"]
+	d = model["eigenvalues"]
+	v = model["eigenvectors"]
+	u = model["u"]
+
+	fs = real(sqrt.(complex(d)))/(2*pi)
+	println("Dry Eigenvalues: $(sigdig.(2*pi*fs)) [radians per second]")
+	println("Dry Natural frequencies: $(sigdig.(fs)) [Hz]")
+
+	neigvs = size(v, 2)
+
+	fes = femm.integdomain.fes
+
+	bfes = meshboundary(fes)
+	# These are the wet surface panels
+	wbfes = bfes;
+	nnperel = nodesperelem(wbfes)
+	
+	pnormals = LaplBEM.allnormals(LaplBEM.allelxs(fens.xyz, connasarray(wbfes)))
+	vn = fill(0.0, count(wbfes), neigvs)
+	uk = fill(0.0, 3)
+	for mode in 1:neigvs
+		for i in 1:count(wbfes)
+			vn[i, mode] = 0.0
+			for k in wbfes.conn[i]
+				for m in 1:3
+					dof = u.dofnums[k, m]
+					uk[m] = (dof != 0 ? v[dof, mode] : 0.0)
+				end
+				vn[i, mode] += dot(pnormals[i], uk)
+			end
+			vn[i, mode] /= nnperel
+		end
+	end
+	
+	scalars = []
+	for mode in 1:neigvs
+		push!(scalars, ("vn$mode", deepcopy(vn[:, mode])))
+	end
+	# File =  "flint_rock3_wet-vn.vtk"
+	# vtkexportmesh(File, fens, wbfes; scalars=scalars)
+	# @async run(`"paraview.exe" $File`)
+
+	println("Layer potential matrices")
+	@timeit to "Layer potential matrices" begin
+		A = fill(0.0, count(wbfes), count(wbfes))
+		A = LaplBEM.doublelayer!(A, fens.xyz, connasarray(wbfes), TriRule(3)) 
+		B = fill(0.0, count(wbfes), count(wbfes))
+		B = LaplBEM.singlelayer!(B, fens.xyz, connasarray(wbfes), TriRule(3), TriRule(3)) 
+	end
+	println("Solve BEM equations")
+	@timeit to "Solve BEM equations" begin
+		pp = A \ (B * vn) 
+	end
+
+	areas = fill(0.0, count(wbfes))
+	n = fill(0.0, 3)
+	for i in 1:count(wbfes)
+		ix = collect(wbfes.conn[i])
+		LaplBEM.trinml!(n, @view fens.xyz[ix, :])
+		areas[i] = norm(n) / 2
+	end
+
+	rK = diagm(vec(d))    
+	rM = I    
+	raM = fill(0.0, neigvs, neigvs);
+	println("Added mass matrix")
+	@timeit to " " begin
+		for i in 1:neigvs
+		    for j in 1:neigvs
+		        raM[i,j] = rhow*dot(vn[:,i].*areas, pp[:,j]);
+		    end
+		end
+		raM = (raM +raM')/2;# it should be symmetric, but make sure
+	end
+
+	decomp = eigen(rK, rM+raM)
+	wv = v*decomp.vectors;
+	d = decomp.values
+
+	@show to
+
+	fs = real(sqrt.(complex(d)))/(2*pi)
+	println("Wet Eigenvalues: $(sigdig.(2*pi*fs)) [radians per second]")
+	println("Wet Natural frequencies: $(sigdig.(fs)) [Hz]")
+
+	true
+end # flint_rock3_wet
 
 function allrun()
     println("#####################################################")
