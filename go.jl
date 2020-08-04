bb0a0db291bb2731f57469e800ec0069efe5310b
Petr Krysl <pkrysl@ucsd.edu>
2020-08-03 19:59:02 -0700

    return to top folder

--- go.jl
+++ go.jl
@@ -1,6 +1,8 @@
 using Pkg; Pkg.activate("."); Pkg.instantiate()
+top = pwd()
 cd("./examples")    
 include(joinpath(pwd(), "./flint_rock3_examples.jl"));
 d2 =  flint_rock3_examples.free_vibration_solver_w_remeshing_ren(6, 2)
 @show d2["eigenvalues"]
+cd(top)    
 
