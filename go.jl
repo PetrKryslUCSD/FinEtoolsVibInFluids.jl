41f8f42edea07df40fc04a9e3937185eaeb92c42
Petr Krysl <pkrysl@ucsd.edu>
2020-08-03 19:33:10 -0700

    driver

--- go.jl
+++ go.jl
@@ -1,4 +1,6 @@
 using Pkg; Pkg.activate("."); Pkg.instantiate()
-cd("C:\\Users\\PetrKrysl\\Documents\\work\\FinEtoolsVibInFluids.jl\\examples")                                           
-include("C:\\Users\\PetrKrysl\\Documents\\work\\FinEtoolsVibInFluids.jl\\examples\\flint_rock3_examples.jl");
-d2 =  flint_rock3_examples.free_vibration_solver_w_remeshing_ren(6, 2)
+cd("./examples")    
+include(joinpath(pwd(), "./flint_rock3_examples.jl"));
+@show d2 =  flint_rock3_examples.free_vibration_solver_w_remeshing_ren(6, 2)
+using JSON
+savejson("free_vibration_solver_w_remeshing_ren-results.json", d2)
